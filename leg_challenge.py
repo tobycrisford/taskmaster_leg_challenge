4dd0cb570a7b305788210a5ae4f33f0677e59715
Toby Crisford <tobycrisford@gmail.com>
2024-10-04 08:08:26 +0100

    Solving game through multiple rounds

--- leg_challenge.py
+++ leg_challenge.py
@@ -16,7 +16,31 @@ def payoff_matrix(a_legs: np.ndarray, b_legs: np.ndarray, target: int):
 
 
 
-def solve_game(a_legs: np.ndarray, b_legs: np.ndarray, target: int):
+def solve_game(a_legs: np.ndarray, b_legs: np.ndarray, target: int, n_rounds: int):
 
-    rps = nash.Game(payoff_matrix(a_legs, b_legs, target))
-    return rps.support_enumeration()
+    if n_rounds == 1:
+        matrix = payoff_matrix(a_legs, b_legs, target)
+    elif n_rounds > 1:
+        matrix = np.zeros((len(a_legs), len(b_legs)))
+        a_sum = np.sum(a_legs)
+        b_sum = np.sum(b_legs)
+        for i in range(len(a_legs)):
+            for j in range(len(b_legs)):
+                a_throw = a_legs[i]
+                b_throw = b_legs[j]
+                if target - a_sum == b_throw - a_throw:
+                    matrix[i, j] = 1.0
+                elif b_sum - target == b_throw - a_throw:
+                    matrix[i, j] = -1.0
+                else:
+                    a_legs[i] = b_throw
+                    b_legs[j] = a_throw
+                    matrix[i, j] = solve_game(a_legs, b_legs, target, n_rounds - 1)[0]
+                    a_legs[i] = a_throw
+                    b_legs[j] = b_throw
+    else:
+        raise NotImplementedError()
+    
+    rps = nash.Game(matrix)
+    eq_strategies = list(rps.support_enumeration())
+    return rps[eq_strategies[0][0], eq_strategies[0][1]][0], eq_strategies
